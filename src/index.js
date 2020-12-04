28228e5775229889b00f59ffbd46ba82281d50cf
GrapeColor <grapecolor@users.noreply.github.com>
2020-12-05 01:36:04 +0900

    Add presence

--- src/index.js
+++ src/index.js
@@ -2,15 +2,21 @@ import { Client, Intents } from 'discord.js';
 
 const bot = new Client({ ws: { intents: Intents.NON_PRIVILEGED } });
 
-console.log(process.env['EXPERIMENT_ENV']);
-
 bot.on('message', message => {
   if (message.content = 'ping!') message.channel.send('pong!');
 });
 
+bot.on('ready', () => {
+  console.info('Ping Pong have logged in to Discord.');
+
+  bot.user?.setPresence({
+    activity: {
+      type: 'PLAYING',
+      name: '自動デプロイ実験中',
+    }
+  });
+});
+
 bot.login(process.env['PING_PONG_TOKEN'])
-  .then(() => {
-    process.on('SIGTERM', bot.destroy);
-    console.info('Ping Pong have logged in to Discord.');
-  })
+  .then(process.on('SIGTERM', bot.destroy))
   .catch(console.error);

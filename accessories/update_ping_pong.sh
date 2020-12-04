40f72aa12417419f666e67043c32273db2aeebc5
GrapeColor <grapecolor@users.noreply.github.com>
2020-12-05 01:56:46 +0900

    Fix passing environment variables

--- accessories/update_ping_pong.sh
+++ accessories/update_ping_pong.sh
@@ -10,6 +10,7 @@ docker-compose down --rmi all
 git pull origin master
 docker-compose build --no-cache
 docker-compose up --no-start
+echo "Completed updating Ping Pong"
 
 sudo service ping_pong start
 echo "Ping Pong has started"

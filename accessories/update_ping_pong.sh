ae3b54925fae3d2dad3ffeb4040295236b011cd0
GrapeColor <grapecolor@users.noreply.github.com>
2020-12-05 00:56:35 +0900

    Update deployment system

--- accessories/update_ping_pong.sh
+++ accessories/update_ping_pong.sh
@@ -6,7 +6,8 @@ echo "Ping Pong has stopped"
 cd ~/bots/ping_pong
 docker-compose down --rmi all
 git pull origin master
-docker-compose build
+docker-compose build --no-cache
+docker-compose up --no-start
 
 sudo service ping_pong start
 echo "Ping Pong has started"

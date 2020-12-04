61a4817bb32dbd40f498445c670dd7c5cd4b8080
GrapeColor <grapecolor@users.noreply.github.com>
2020-12-05 01:40:32 +0900

    Fix hostname and more

--- accessories/update_ping_pong.sh
+++ accessories/update_ping_pong.sh
@@ -4,7 +4,9 @@ sudo service ping_pong stop
 echo "Ping Pong has stopped"
 
 cd ~/bots/ping_pong
+
 docker-compose down --rmi all
+
 git pull origin master
 docker-compose build --no-cache
 docker-compose up --no-start

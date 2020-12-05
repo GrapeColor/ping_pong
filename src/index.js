f425fda0c42738992f8815d1793989b4609bc75b
GrapeColor <grapecolor@users.noreply.github.com>
2020-12-05 18:39:32 +0900

    Add the command to search tweets

--- src/index.js
+++ src/index.js
@@ -1,24 +1,47 @@
 import { Client, Intents } from 'discord.js';
+import Twitter from 'twitter';
+
+const commitHash = process.env['COMMIT_HASH'];
+
+const twitter = new Twitter({
+  consumer_key       : process.env['TWITTER_CONSUMER_KEY'],
+  consumer_secret    : process.env['TWITTER_CONSUMER_SECRET'],
+  access_token_key   : process.env['TWITTER_ACCESS_TOKEN'],
+  access_token_secret: process.env['TWITTER_ACCESS_TOKEN_SECRET'],
+});
 
 const bot = new Client({ ws: { intents: Intents.NON_PRIVILEGED } });
 
-bot.on('message', message => {
-  if (!message.author.bot && message.content === 'ping!')
-    message.channel.send('pong!')
-      .catch(console.error);
-});
-
 bot.on('ready', () => {
-  console.info('Ping Pong have logged in to Discord.');
+  process.on('exit', bot.destroy);
+  console.info('Ping Pong has logged in to Discord.');
 
   bot.user?.setPresence({
     activity: {
       type: 'PLAYING',
-      name: '自動デプロイ実験中',
+      name: `現在のコミット: ${commitHash?.slice(0, 7) ?? '不明'}`,
     }
   });
 });
 
+bot.on('message', message => {
+  if (message.content === 'ping!')
+    message.channel.send('pong!')
+      .catch(console.error);
+
+  const match = message.content.match(/^\/tweets (.+)/);
+  if (match)
+    twitter.get('search/tweets', { q: `${match[1]} exclude:retweets`, count: 5 })
+      .then(tweets => {
+        message.channel.send(
+          tweets.statuses
+            .map(tweet => `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`)
+            .join('\n')
+        )
+          .catch(console.error);
+      })
+      .catch(console.error);
+});
+
 bot.login(process.env['PING_PONG_TOKEN'])
-  .then(process.on('SIGTERM', bot.destroy))
   .catch(console.error);

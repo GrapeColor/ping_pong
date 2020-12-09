fdd150112e7647161dd7fb2842eb486b14f90749
GrapeColor <grapecolor@users.noreply.github.com>
2020-12-09 13:07:14 +0900

    Improve updater output

--- accessories/update_ping_pong.sh
+++ accessories/update_ping_pong.sh
@@ -1,16 +1,23 @@
-#!/bin/sh
+#!/bin/sh -x
 
 sudo service ping_pong stop
 echo "Ping Pong has stopped"
+echo "----------------------------------------------------------------"
 
 cd ~/bots/ping_pong
 
 docker-compose down --rmi all
+echo "----------------------------------------------------------------"
 
 git pull origin master
+echo "----------------------------------------------------------------"
+
 docker-compose build --no-cache
+echo "----------------------------------------------------------------"
 docker-compose up --no-start
-echo "Completed updating Ping Pong"
+echo "----------------------------------------------------------------"
+
+echo "Ping Pong has completed the update"
 
 sudo service ping_pong start
 echo "Ping Pong has started"

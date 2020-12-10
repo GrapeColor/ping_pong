510a5f54b87db512ded588e35fc7a1273ba1652d
GrapeColor <grapecolor@users.noreply.github.com>
2020-12-11 05:21:14 +0900

    Add sharp to information log

--- accessories/update_ping_pong.sh
+++ accessories/update_ping_pong.sh
@@ -6,20 +6,20 @@ git pull origin master
 echo "----------------------------------------------------------------"
 docker-compose build --no-cache
 echo "----------------------------------------------------------------"
-echo "Ping Pong has completed the update."
+echo "# Ping Pong has completed the update."
 
 sudo service ping_pong stop
-echo "Ping Pong has stopped."
+echo "# Ping Pong has stopped."
 echo "----------------------------------------------------------------"
 
 docker-compose up --no-start
 echo "----------------------------------------------------------------"
-echo "Ping Pong has recreated the container."
+echo "# Ping Pong has recreated the container."
 echo "----------------------------------------------------------------"
 
 docker image prune -f
 echo "----------------------------------------------------------------"
-echo "Remove unused images of Docker."
+echo "# Remove unused images of Docker."
 
 sudo service ping_pong start
-echo "Ping Pong has restarted."
+echo "# Ping Pong has restarted."

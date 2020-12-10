6c48737d429065c7697cf0aa7db7e3da466596fc
GrapeColor <grapecolor@users.noreply.github.com>
2020-12-11 03:52:43 +0900

    Change to create container after stopping service

--- accessories/update_ping_pong.sh
+++ accessories/update_ping_pong.sh
@@ -6,12 +6,15 @@ git pull origin master
 echo "----------------------------------------------------------------"
 docker-compose build --no-cache
 echo "----------------------------------------------------------------"
-docker-compose up --no-start
-echo "----------------------------------------------------------------"
 echo "Ping Pong has completed the update."
 
 sudo service ping_pong stop
 echo "Ping Pong has stopped."
+
+echo "----------------------------------------------------------------"
+docker-compose up --no-start
+echo "----------------------------------------------------------------"
+echo "Ping Pong has created the container."
 
 sudo service ping_pong start
 echo "Ping Pong has restarted."

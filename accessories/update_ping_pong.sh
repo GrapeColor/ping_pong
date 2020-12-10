e07f2d1918e0cb0e5c55ac70b43c1060d3c52f2d
GrapeColor <grapecolor@users.noreply.github.com>
2020-12-11 03:05:15 +0900

    Update the script on the production server side

--- accessories/update_ping_pong.sh
+++ accessories/update_ping_pong.sh
@@ -1,23 +1,22 @@
 #!/bin/sh
 
-sudo service ping_pong stop
-echo "Ping Pong has stopped."
-echo "----------------------------------------------------------------"
-
 cd ~/bots/ping_pong
-
-docker-compose down --rmi all
-echo "----------------------------------------------------------------"
 
 git pull origin master
 echo "----------------------------------------------------------------"
-
 docker-compose build --no-cache
 echo "----------------------------------------------------------------"
 docker-compose up --no-start
 echo "----------------------------------------------------------------"
-
 echo "Ping Pong has completed the update."
 
+sudo service ping_pong stop
+echo "Ping Pong has stopped."
+
 sudo service ping_pong start
-echo "Ping Pong has started."
+echo "Ping Pong has restarted."
+echo "----------------------------------------------------------------"
+
+docker system prune -fa --volumes
+echo "----------------------------------------------------------------"
+echo "Pruned unwanted Docker images and other data."
